8ebcadce9bfb51a72473ea4b1388ff65b0a8de06
SaurabhKante <117333587+SaurabhKante@users.noreply.github.com>
2024-08-08 18:10:11 +0530

    UserAttendance

--- routes/user-attendance/v1/userAttendance_router.js
+++ routes/user-attendance/v1/userAttendance_router.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
-const {postUserAttendance, updateUserAttendance,getUserAttendance} = require("../../../controllers/user-attendance/user-attendance-controller");
+const {
+  postUserAttendance,
+  updateUserAttendance,
+  getUserAttendance,
+//   vehicleModelDropdown,
+} = require("../../../controllers/user-attendance/user-attendance-controller");
 
 router.post("/post-user-attendance", postUserAttendance);
 router.post("/update-user-attendance", updateUserAttendance);
 router.post("/get-user-attendance", getUserAttendance);
+
+// router.post("/vehicle-model", vehicleModelDropdown);
 
 module.exports = router;
